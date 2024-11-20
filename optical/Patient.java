9044184e4f8e0a43d9187397777a8ec48f7704c3
CorpoFechado <randreiromeo@gmail.com>
2024-11-21 01:29:31 +0800

    null

---
+++ optical/Patient.java
@@ -0,0 +1,49 @@
+
+package optical;
+
+public class Patient {
+    
+    private String PatientName, Email;
+    private int PatientID, ContactNumber;
+    
+    // Setters
+    void setName(String PatientName){
+        this.PatientName = PatientName;
+    } 
+    void setEmail(String Email){
+        this.Email = Email;
+    }
+    void setID(int PatientID){
+        this.PatientID = PatientID;
+    }
+    void setNumber(int ContactNumber){
+        this.ContactNumber = ContactNumber;        
+    }
+    
+    // Getters   
+    String getName(){
+        return PatientName;
+    }
+    String getEmail(){
+        return Email;
+    }
+    int setID(){
+        return PatientID;
+    }
+    int setNumber(){
+        return ContactNumber;
+    }
+    
+    // Methods
+    void bookAppointmet(){
+        
+    }
+    void cancelAppointment(){
+        
+    }
+    void buyProduct(){
+        
+    }
+    
+    
+}
